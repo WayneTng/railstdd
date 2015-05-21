331baa1dcb94c9059922fb94280f29bcb21f6c5a
wayne <wayne@futureworkz.com>
2015-05-21 12:01:08 +0700

    #make_title_downcase and #remove_html_tag_from_description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,18 @@
 class Product < ActiveRecord::Base
   belongs_to :category
-  validates :title, :description,  presence: true
+  
+  before_validation :strip_html_from_description
+  before_validation :make_title_lowercase
+
+  validates :title, :description, :category, :price, presence: true
   validates_numericality_of :price, greater_than: 0
+  validate :strip_html_from_description 
+
+  def strip_html_from_description
+    self.description = ActionView::Base.full_sanitizer.sanitize(self.description)
+  end
+
+  def make_title_lowercase
+    self.title = self.title.downcase if self.title.present?
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,10 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:descriotion) }
-  it { should validate_presence_of(:price) }
-  it { should validate_numericality_of(:price).is_greater_than(0) }
-  it { should belong_to(:category) }
+  describe 'Validation' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:category) }
+    it { should validate_numericality_of(:price).is_greater_than(0) }
+  end
 
- end
+  context 'Association' do
+    it { should belong_to(:category) }
+  end
+
+  describe '#strip_html_from_description before validating' do
+    let!(:product){Product.new(description: '<p>ruby book</p>')} 
+    it 'should not contain html tag in description before validating' do
+      product.validate
+      expect(product.description).to eq 'ruby book'
+    end
+  end
+
+  describe '#make_title_lowercase' do
+    let!(:product){Product.new(title: 'RUBY')}
+
+    it 'should make the title lower case before validating' do
+      product.validate
+    expect(product.title).to eq 'ruby'
+    end
+  end
+end

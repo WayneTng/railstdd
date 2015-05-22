5a37ac4018513a934594bd375a1b3c5c9fd7aeb0
wayne <wayne@futureworkz.com>
2015-05-22 14:33:59 +0700

    add features edit into capybara

--- spec/features/user_can_manage_products_spec.rb
+++ spec/features/user_can_manage_products_spec.rb
@@ -2,18 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'User can manage products', type: :feature do
  
-  let!(:product) { build(:product) }
-  let!(:category_title) { product.category.title }
-  it 'can create a product' do
-    visit '/products'
-    expect(page).not_to have_content product.title
-    
-    click_on 'Create a product'
-    fill_in 'Title',        with: product.title
-    fill_in 'Description',  with: product.description
-    fill_in 'Price',        with: product.price
-    select  category_title, from: 'Category'
-    click_on 'Create Product'
-    expect(page).to have_content product.title.downcase
+  describe 'create a product' do
+    let!(:product) { build(:product) }
+    let!(:category_title) { product.category.title }
+    it 'can create a product' do
+      visit '/products'
+      expect(page).not_to have_content product.title
+      
+      click_on 'Create a product'
+      fill_in 'Title',        with: product.title
+      fill_in 'Description',  with: product.description
+      fill_in 'Price',        with: product.price
+      select  category_title, from: 'Category'
+      click_on 'Create Product'
+      expect(page).to have_content product.title.downcase
+    end
+  end
+
+  describe 'edit a product' do
+    let!(:product){ create(:product) }
+    it 'can edit a product' do
+      visit '/products'
+      expect(page).to have_content product.title
+
+      click_on 'Edit'
+      fill_in 'Title', with: 'Java Book'
+      click_on 'Update Product'
+      ap page.body
+      expect(find_field('Title').value).to have_content 'java book'
+    end
   end
 end

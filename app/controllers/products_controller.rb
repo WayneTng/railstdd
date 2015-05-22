2a41b4615afff987df38fe79f53d4d2f8f8fb397
wayne <wayne@futureworkz.com>
2015-05-22 09:51:41 +0700

    add create controller for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,21 @@ class ProductsController < ApplicationController
     render :new
   end
 
+  def create
+    product_params = product_information
+    @product = Product.new(product_params) 
+    if @product.save
+      redirect_to products_url
+    else
+      render :new
+    end
+  end
+
   def product_id
     params.require(:id)
+  end
+
+  def product_information
+    params.require(:product).permit(:title, :description, :price, :published, :category_id)
   end
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -33,4 +33,32 @@ RSpec.describe ProductsController, type: :controller do
       expect(assigns(:product).id).to eq product.id
     end
   end
+
+  describe '#create' do
+    def do_request
+      post :create, product: params
+    end
+
+    context 'success' do
+      let(:params) { attributes_for(:product, category_id: create(category)) }
+      
+      it 'saves a product' do
+        expect { do_request }.to change(Product, :count).by(1)
+      end
+
+      it 'redirects to index on success' do
+        do_request
+        expect(response).to redirect_to products_url
+      end
+    end
+
+    context 'failure' do
+      let(:params){ attributes_for(:product, title: '')}
+
+      it 'renders new on failure' do
+        do_request
+        expect(response).to render_template :new
+      end
+    end
+  end
 end

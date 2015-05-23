88a8b902afa4a82ec90b017abb5d8e5b048bdb6c
wayne <wayne@futureworkz.com>
2015-05-23 10:59:08 +0700

    add images validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   
+  has_attached_file :image, styles: {medium: "300x300", thumb: "100x100"},
+                            default_url: "/assets/images/products/:basename.:extension",
+                            path: ":rails_root/public/assets/images/products/:basename.:extension" 
+
   before_validation :strip_html_from_description
   before_validation :make_title_lowercase
 
@@ -8,6 +12,10 @@ class Product < ActiveRecord::Base
   validates_numericality_of :price, greater_than: 0
   validate :strip_html_from_description 
   validate :title_shorter_than_description
+  #validates_attachment :image, content_type: {content_type: ["image/jpg", "image/gif", "image/png"]},
+  #validates_attachment_presence :image
+  validates_attachment_content_type :image, content_type: {content_type: ["image/png", "image/jpeg", "image/gif"]}
+  validates_attachment_size :image, in: 0..3.megabytes, message: "file size cannot be larger than 3MB"
 
   def strip_html_from_description
     self.description = ActionView::Base.full_sanitizer.sanitize(self.description)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521032158) do
+ActiveRecord::Schema.define(version: 20150523031035) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -22,11 +22,15 @@ ActiveRecord::Schema.define(version: 20150521032158) do
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.decimal  "price",       precision: 10, scale: 2
+    t.decimal  "price",              precision: 10, scale: 2
     t.boolean  "published"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id"
